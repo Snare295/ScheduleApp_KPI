42a79b54aeb53a6c0d8381a08ce405151413d356
Snare295 <trololoska2325@gmail.com>
2022-06-30 02:36:21 +0300

    throughout commit

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:scheduleapp_kpi/main.dart';
+import 'package:first_app/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
